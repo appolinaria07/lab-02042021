8fb0defd542e687c1ed1bfb060eca3021e2d8e5e
appolinaria07 <77410102+appolinaria07@users.noreply.github.com>
2021-02-05 23:54:34 -0800

    init

---
+++ scripts.js
@@ -0,0 +1,14 @@
+/*
+App: First JavaScript Application
+Author: Polina Kochetkova
+Date: 02/04/2021
+*/
+//Allow the user to enter their guess
+let num = prompt('Guess a number between 1 and 10');
+// Check to see if the number is a winner
+if (num ==5) {
+   alert('You guessed the righe number!');
+} else {
+    // If it isn't, they got it wrong
+    alert('You guessed the wrong number!');
+}
