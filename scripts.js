e131894234fd0e8f8101716f152f9fdc73a97ca9
appolinaria07 <77410102+appolinaria07@users.noreply.github.com>
2021-02-06 00:09:16 -0800

    back to 5

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ Date: 02/04/2021
 //Allow the user to enter their guess
 let num = prompt('Guess a number between 1 and 10');
 // Check to see if the number is a winner
-if (num ==7) {
+if (num ==5) {
    alert('You guessed the right number!');
 } else {
     // If it isn't, they got it wrong
